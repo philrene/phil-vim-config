1b285e549e3fb139e4ec27d6a8e1f0506ee39295
Phil Rene <prene035@gmail.com>
2011-05-22 14:32:25 -0400

    fix script to install

--- philsconfig.sh
+++ philsconfig.sh
@@ -7,3 +7,8 @@ git clone https://github.com/vim-scripts/pathogen.vim.git
 cp ./pathogen.vim/plugin/pathogen.vim ../.vim/autoload/pathogen.vim
 cd ..
 rm -rf tmp
+
+git submodule init
+git submodule update
+
+cp -Rf .vim .vimrc ~/
