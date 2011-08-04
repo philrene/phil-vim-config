1ba9ba0c4ccecb45a8ed1100716679c4fa776605
prene <prene@boo.boo.lan>
2011-08-04 16:02:43 -0400

    Small but in philsconfig.sh

--- philsconfig.sh
+++ philsconfig.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+git submodule init
+git submodule update
+
 
 [ -z tmp ] || mkdir tmp
 cd  tmp
 git clone https://github.com/vim-scripts/pathogen.vim.git
+[ -z ../.vim/autoload ] || mkdir ../.vim/autoload/
 cp ./pathogen.vim/plugin/pathogen.vim ../.vim/autoload/pathogen.vim
 cd ..
 rm -rf tmp
-
-git submodule init
-git submodule update
 
 cp -Rf .vim .vimrc ~/
