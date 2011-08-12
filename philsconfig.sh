70c86cfa523a1d0cbe49e1a20896afc3e3b0761f
Phil Rene <prene035@gmail.com>
2011-08-12 15:18:08 -0400

    Modded philsconfig.sh and added autoload dir

--- philsconfig.sh
+++ philsconfig.sh
@@ -7,7 +7,6 @@ git submodule update
 [ -z tmp ] || mkdir tmp
 cd  tmp
 git clone https://github.com/vim-scripts/pathogen.vim.git
-[ -z ../.vim/autoload ] || mkdir ../.vim/autoload/
 cp ./pathogen.vim/plugin/pathogen.vim ../.vim/autoload/pathogen.vim
 cd ..
 rm -rf tmp
